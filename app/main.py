28fa6698cc031f7aa84e74585b0d1105dc41f0b7
Rodrigo Obando <rfnobando@gmail.com>
2026-03-29 23:14:16 -0300

    feat: add dockerignore

--- app/main.py
+++ app/main.py
@@ -5,13 +5,9 @@ from app.database import engine
 
 app = FastAPI()
 
-origins = [
-    "http://localhost:10002"
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
